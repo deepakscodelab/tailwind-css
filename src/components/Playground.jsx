c4261e44518aacfb1427b4278569d4512b95ea14
Deepak Sharma <ce.deepaksharma@gmai.com>
2024-08-04 10:21:59 +0530

    Update git usernmae

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,5 +1,5 @@
 export default function Playground() {
   return (
-    <div className="h-80 w-80 bg-pink-400 text-center">Hello Playground</div>
+    <div className="h-80 w-80 bg-violet-400 text-center">Hello Playground</div>
   );
 }
